b55377f4c24f667ff9b5271d3bd19db825f1c668
Niatomi <playervoker@gmail.com>
2021-12-25 23:50:39 +0400

    Add reading from test data file

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,55 @@
 import api.ParserEditor;
-import api.ParserEditorImp;
+import api.ParserEditorImpl;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         //init
-        ParserEditor parserEditor = new ParserEditorImp();
+        ParserEditor parserEditor = new ParserEditorImpl();
+        List<String> listOfNames = new ArrayList<>();
 
-        String example1 = "";
-        String example2 = "Peter";
-        String example3 = "Sam, Peter";
-        String example4 = "Jackson, Peter, Sam, Peter1";
+        Path path = Paths.get("")
+                .toAbsolutePath()
+                .resolve("src")
+                .resolve("main")
+                .resolve("resources")
+                .resolve("NAMES_DATA.csv");
 
-        System.out.println(parserEditor.showLiked(example1));
-        System.out.println(parserEditor.showLiked(example2));
-        System.out.println(parserEditor.showLiked(example3));
-        System.out.println(parserEditor.showLiked(example4));
+        List<String> bag =
+                Files.lines(path, StandardCharsets.UTF_8)
+                    .flatMap(line -> Arrays.stream(line.split("\\n+")))
+                    .collect(Collectors.toList());
+        bag.remove(0);
+
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (int i = 0; i < bag.size(); i++) {
+            if ((int)(Math.random()*2) == 1) {
+                if (stringBuilder.toString() == "") {
+                    stringBuilder.append(bag.get(i));
+                } else {
+                    stringBuilder.append(", " + bag.get(i));
+                }
+            } else if (stringBuilder.toString() != ""){
+                listOfNames.add(stringBuilder.toString());
+                stringBuilder.setLength(0);
+            }
+        }
+
+        for (String s : listOfNames) {
+            System.out.println(parserEditor.showLiked(s));
+        }
 
     }
 }
